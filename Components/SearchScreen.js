5c68ae603d6b9ade70e6dd6a53bd5a95eef8e738
Yuri <yuri.shiba@unifesp.br>
2021-03-08 09:18:23 -0300

    Created SearchScreen

---
+++ Components/SearchScreen.js
@@ -0,0 +1,54 @@
+import React from 'react';
+
+import {
+    StyleSheet,
+    Text,
+    View,
+    Image,
+    ImageBackground,
+    TouchableHighlight
+} from 'react-native';
+
+
+export default function HomeScreen() {
+    return (
+        <ImageBackground source={require('../img/imgbground.png')} style={styles.bgImg}>
+            <View style={styles.container}>
+                <Image
+                    source={require('../img/freetogame-logo.png')}
+                    style={styles.logo}
+                />
+
+                <TouchableHighlight style={styles.btnSearch} underlayColor="#750f04">
+                    <Text style={{ color: '#ffff' }}>Search</Text>
+                </TouchableHighlight>
+
+            </View>
+        </ImageBackground>
+    )
+}
+
+const styles = StyleSheet.create({
+    logo: {
+        width: 300,
+        height: 300,
+        alignSelf: 'center',
+        resizeMode: 'contain'
+    },
+    bgImg: {
+        width: '100%',
+        height: '100%'
+    },
+    container: {
+        alignItems: 'center',
+    },
+    btnSearch: {
+        width: 90,
+        margin: 20,
+        padding: 10,
+        alignItems: 'center',
+        borderWidth: 1,
+        borderRadius: 5,
+        backgroundColor: '#b51c0b'
+    }
+})
