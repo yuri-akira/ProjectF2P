19a5eb11eadf3fe4bf9e2a6bdbd7e9b712dd0792
Yuri <yuri.shiba@unifesp.br>
2021-03-12 12:00:18 -0300

    Added DetailsScreen to navigation

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import SearchScreen from './Screens/SearchScreen';
 import ResultScreen from './Screens/ResultsScreen';
+import DetailsScreen from './Screens/DetailsScreen';
 
 const Stack = createStackNavigator();
 
@@ -20,6 +21,10 @@ export default function App() {
         <Stack.Screen
           name="Results"
           component={ResultScreen}
+        />
+        <Stack.Screen
+          name="Details"
+          component={DetailsScreen}
         />
       </Stack.Navigator>
     </NavigationContainer>
